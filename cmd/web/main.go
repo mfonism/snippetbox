e5925f47e64040c55b6e48f07720e4d4d6e203f8
Mfon Eti-mfon <mfonetimfon@gmail.com>
2023-09-10 12:53:05 +0100

    Create custom filesystem and use it to disable directory listing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	mux.Handle("/static", http.NotFoundHandler())
 	// we need to strip the `/static` prefix off of the URL
 	// before sending it into the file server
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -20,4 +22,30 @@ func main() {
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
