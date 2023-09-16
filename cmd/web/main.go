863a36c82138ff3f2e8d4152dfd9eda9d6a696e0
Mfon Eti-mfon <mfonetimfon@gmail.com>
2023-09-17 00:08:33 +0100

    Refactor custom filesystem into dedicated file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 type application struct {
@@ -34,30 +33,4 @@ func main() {
 	app.infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	app.errorLog.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
