8fb58def6080804d129e860baabe21ea326d88ff
Mfon Eti-mfon <mfonetimfon@gmail.com>
2023-09-17 11:48:43 +0100

    Set-up a MySQL connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +18,11 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":666", "HTTP network address")
+
+	dbPass := os.Getenv("dbPass")
+	defaultDSN := fmt.Sprintf("web:%s@/snippetbox?parseTime=true", dbPass)
+	dsn := flag.String("dsn", defaultDSN, "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -24,6 +33,13 @@ func main() {
 		infoLog: infoLog,
 	}
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: app.errorLog,
@@ -31,6 +47,20 @@ func main() {
 	}
 
 	app.infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
