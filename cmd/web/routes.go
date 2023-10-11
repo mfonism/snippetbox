e0eaa51a43e51ed0b7485df765d83efc9863cda2
Mfon Eti-mfon <mfonetimfon@gmail.com>
2023-10-11 23:16:41 +0100

    Use the package 'julienschmidt/httprouter' to implement clean URLs and method-based routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,21 +3,22 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
 
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	middlewareChain := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return middlewareChain.Then(mux)
+	return middlewareChain.Then(router)
 }
