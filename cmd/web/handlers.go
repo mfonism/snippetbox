7054fb7c09fe7461910dc2b62412afdbbe256e62
Mfon Eti-mfon <mfonetimfon@gmail.com>
2023-10-21 23:18:04 +0100

    Implement flash messages upon/with the session manager

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,8 +44,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	flash := app.sessionManager.PopString(r.Context(), "flash")
+
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
+	data.Flash = flash
 
 	app.render(w, http.StatusOK, "view.tmpl", data)
 }
@@ -106,6 +109,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
